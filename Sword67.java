83c939b1bb9ef9719a89d40641c598393473f2dc
Sue Admin <sueadmin@Adrians-MacBook-Air-2.local>
2022-07-05 12:29:57 -0400

    test

--- Sword67.java
+++ Sword67.java
@@ -1,11 +1,11 @@
 public class Sword67 {
     /*
      * https://leetcode.com/problems/search-a-2d-matrix/
-     * 
+     * https://leetcode.com/problems/search-a-2d-matrix-ii/
      * From top right to lower left, then only has 2 options to grow or shrink O(n)
      * Binary. left=0,right=m*n-1; getMidNumber=matrix[mid/n][mid%n], regardless of m
      * 
-     * Search2dMatrix74.java
+     * https://github.com/HuiHuangSue/lc/blob/main/library/Search2dMatrix74_240.java 
     */
     
 
