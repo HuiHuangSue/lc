37f77e9d9919270524317cf418b88bdbecf0f172
Sue Admin <sueadmin@Adrians-MacBook-Air-2.local>
2022-10-29 23:07:00 -0400

    update

---
+++ A-library/LLCycle141142.java
@@ -0,0 +1,30 @@
+public class LLCycle141142 {
+    public boolean hasCycle141(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ListNode detectCycle142(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head; // fast放最前，当fast和slow再次相遇的时候是节点
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
+}
